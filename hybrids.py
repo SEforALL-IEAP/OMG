870ae9023f8f0f05141d31577ee53575a368e94f
Andreas Sahlberg <asahl@kth.se>
2024-09-03 16:25:50 +0200

    Updated PV retrieval

--- hybrids.py
+++ hybrids.py
@@ -370,34 +370,37 @@ def get_pv_data(latitude, longitude, token, output_folder):
         'raw': True
     }
 
-    if token != '':
-
-        try:
-            r = s.get(url, params=args)
-
-            # Parse JSON to get a pandas.DataFrame of data and dict of metadata
-            parsed_response = json.loads(r.text)
-
-        except json.decoder.JSONDecodeError:
-            print('API maximum hourly requests reached, waiting one hour', time.ctime())
-            time.sleep(3700)
-            print('Wait over, resuming API requests', time.ctime())
-            r = s.get(url, params=args)
-
-            # Parse JSON to get a pandas.DataFrame of data and dict of metadata
-            parsed_response = json.loads(r.text)
-
-        data = pd.read_json(StringIO(json.dumps(parsed_response['data'])), orient='index')
-
-        df_out = pd.DataFrame(columns=['time', 'ghi', 'temp'])
-        df_out['ghi'] = (data['irradiance_direct'] + data['irradiance_diffuse']) * 1000
-        df_out['temp'] = data['temperature']
-        df_out['time'] = data['local_time']
-
-        df_out.to_csv(out_path, index=False)
-        
+    if os.path.exists(out_path):
+        print('Data already in the PV folder')
     else:
-        print('No token provided')
+        if token != '':
+    
+            try:
+                r = s.get(url, params=args)
+    
+                # Parse JSON to get a pandas.DataFrame of data and dict of metadata
+                parsed_response = json.loads(r.text)
+    
+            except json.decoder.JSONDecodeError:
+                print('API maximum hourly requests reached, waiting one hour', time.ctime())
+                time.sleep(3700)
+                print('Wait over, resuming API requests', time.ctime())
+                r = s.get(url, params=args)
+    
+                # Parse JSON to get a pandas.DataFrame of data and dict of metadata
+                parsed_response = json.loads(r.text)
+    
+            data = pd.read_json(StringIO(json.dumps(parsed_response['data'])), orient='index')
+    
+            df_out = pd.DataFrame(columns=['time', 'ghi', 'temp'])
+            df_out['ghi'] = (data['irradiance_direct'] + data['irradiance_diffuse']) * 1000
+            df_out['temp'] = data['temperature']
+            df_out['time'] = data['local_time']
+    
+            df_out.to_csv(out_path, index=False)
+            
+        else:
+            print('No token provided')
 
 
 def read_environmental_data(path, skiprows=24, skipcols=1):
